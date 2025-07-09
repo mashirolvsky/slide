76259b8a8945673616820616e666e4e10c8aa86d
Lucas de Andrade Rocha <lucas@ecombase.com.br>
2025-07-09 11:30:27 -0300

    updt-distance: Criação dos calculos de distâncias dos eventos para os slides

--- src/modules/slide.js
+++ src/modules/slide.js
@@ -2,20 +2,36 @@ export default class Slide {
   constructor(wrapper, slide) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
   }
+  moveSlide(posiX) {
+    this.dist.movedPosition = posiX;
+    this.slide.style.transform = `translate3d(${posiX}px, 0, 0)`;
+  }
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.84;
+    return this.dist.finalPosition - this.dist.movement;
+  }
+
   onStart(e) {
     e.preventDefault();
-    console.log("Clicked!");
+    this.dist.startX = e.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onMove(e) {
-    console.log("Moved");
+    const lastPosition = this.updatePosition(e.clientX);
+    this.moveSlide(lastPosition);
   }
 
   onEnd(e) {
     console.log("Ended");
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movedPosition;
   }
   addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart);
